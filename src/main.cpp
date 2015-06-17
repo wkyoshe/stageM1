e2b13a847ebdb36f04ec9d6ddd95f47029f1aa25
Kyoshe Winstone <wkyoshe@gmail.com>
2015-06-17 12:37:10 +0200

    main.cpp update

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,22 @@
 #include <iostream>
-#include “operation.hpp”
-int main ()
-{
-  int num1, num2;
+#include <stdlib.h>
  
-  std::cout << "Enter a number :\n";
-  std::cin >> num1; 
-  std::cout << "Enter another number :\n";
-  std::cin>> num2;
-  int z,r,q,p;
-  z = addition (num1,num2);
-  q =substruction (num1,num2);
-  p =multiplication (num1,num2);
-  r = division (num1,num2);
-  std::cout << "a+b = " << z<<std::endl;
-  std::cout << "a-b = " << q<<std::endl;
-  std::cout << "a*b = " << p<<std::endl;
-  std::cout << "a/b = " << r<<std::endl;
-  return 0;
+using namespace std;
+ 
+#include "operation.h"
+ 
+int main(int argc, char **argv) 
+{ 
+  double x =0, y=0;
+  if(argc == 3){
+    x = atof(argv[1]);
+    y = atof(argv[2]);
+  }
+  operation B(x,y);
+    
+  cout << B.getX() << "+" << B.getY() << "=" << B.addition() << endl;
+  cout << B.getX() << "-" << B.getY() << "=" << B.substruction() << endl;
+  cout << B.getX() << "*" << B.getY() << "=" << B.multiplication() << endl;
+  cout << B.getX() << "/" << B.getY() << "=" << B.division() << endl;
+    return 0;
 }
