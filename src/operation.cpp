6ed804e3a9e87696229a45292d06176f9acea3e6
Kyoshe Winstone <wkyoshe@gmail.com>
2015-06-19 15:29:43 +0200

    update operation.cpp

--- src/operation.cpp
+++ src/operation.cpp
@@ -1,55 +1,98 @@
+/*!
+ * \breif     operation.h
+ * \Date      june 19, 2015
+ * \author:   kyoshe winstone
+ * \version   1.1
+ */  
+     
 #include "operation.h"
 #include <iostream>
 using namespace std;
- 
+
+class operation{
+         public:
+               
+                 //! Default Constructor
+                  /*!
+                    *constructs an empty object
+                   */  
+  
+
 operation::operation() : x(0), y(0)
 {}
- 
+  /**
+   *Constructs an objects with two arguments x and y
+   */
 operation::operation(double x, double y) : x(x), y(y)
 {}
- 
+
+  /**
+   *set the value of x
+   */
 void operation::setX(double x)
 {
-    this->x = x;
+  this->x = x;
 }
  
+  /**
+   *set the value of y
+   */
 void operation::setY(double y)
 {
-    this->y = y;
-}
- 
-double operation::getX() const
-{
-    return this->x;
-} 
- 
-double operation::getY() const
-{
-    return this->y;
-} 
- 
- 
- 
-double operation::addition( )
-{
-return (x + y);
+  this->y = y;
 }
 
+  /**
+   *return the value of x
+   */
+double operation::getX() const
+{
+  return this->x;
+} 
+
+  /**
+   *set the value of y
+   */
+double operation::getY() const
+{
+  return this->y;
+} 
+ 
+
+  /**
+   *returns the sum of two numbers
+   */
+double operation::addition( )
+{
+  return (x + y);
+}
+
+  /**
+   *returns the diff of two numbers
+   */
 double operation::substruction( )
 {
   return (x - y);
 }
+
+  /**
+   *returns the product of two numbers
+   */
 double operation::multiplication( )
 {
   return (x * y);
 }
 
+  /**
+   *returns the quotient of two numbers
+   */
 double operation::division( )
 {if(y != 0)
     {
       return x / y;
 
     }
-  cout << "Error: division by zero.\n";
-  return 0;
+  //cout << "Error: division by zero.\n";
+  //return 0;
+}
 }
