177438cfdae00701b3ae0b82eb8d02a26143ee92
bachir151 <elbachir90@gmail.com>
2015-06-30 15:28:00 +0200

    Update Chaine.h

--- src/Chaine.h
+++ src/Chaine.h
@@ -1,16 +1,37 @@
+/**
+ * @brief     Chaine.h
+ * @date      June 29, 2015
+ * @author    Diedhiou Ahmed Bachir
+ * @version   1.1
+ */
+
 #ifndef CHAINE_H
 #define CHAINE_H
 #include <string>
 using namespace std;
+
+
 class Chaine
 {
-public:
+/**
+ *@param mot is a variable
+ */
+private:
 string mot;
-string Tri( );
+
+/**
+ *Accessors and mutators
+ */
+public:
 string getMot();
-double Lmot (); 
-string Estpalindrom();
+void setMot(string mot);
+
+/**
+ *functions
+ */
+public:
+string Tri( );
+bool Estpalindrom();
 
 
 };
-#endif 
