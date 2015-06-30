0d95bb3df0f869c473e4e8914eac9da921583b81
bachir151 <elbachir90@gmail.com>
2015-06-30 15:23:46 +0200

    Update Chaine.cpp

--- src/Chaine.cpp
+++ src/Chaine.cpp
@@ -1,25 +1,61 @@
+/**
+ * @brief     Chaine.h
+ * @date      June 29, 2015
+ * @author    Diedhiou Ahmed Bachir
+ * @version   1.1
+ */
+ 
 #include <algorithm>
 #include <iostream>
 #include <string>
 #include "Chaine.h"
 using namespace std;
 
-	
-string Chaine ::Tri( ){
- std::sort(mot.begin(), mot.end());
-return (mot);
+
+
+/**
+ *@returns sorted string in alphabetic order
+ */
+string Chaine::Tri( ){
+ 	std::sort(mot.begin(), mot.end());
+	return (mot);
  }
 
-string Chaine :: getMot(){
-return mot ;
+/**
+ *@return the string 
+ */
+ 
+string Chaine::getMot(){
+	return mot ;
 }
 
-string Chaine::Estpalindrom(){
-if(equal(mot.begin(), mot.end(), mot.rbegin()))
-        	return "true" ; 
-	else 
-		return "false" ; 
+
+/**
+ *sets the string 
+ */
+ 
+void Chaine::setMot(string mot){
+	this->mot=mot;
 }
+
+
+/**
+ *return true if string is a palindrome  
+ */
+
+bool Chaine::Estpalindrom(){
+
+Chaine A ;
+A.setMot(mot);
+string tmp=A.getMot();
+if (tmp==A.Tri())
+{ return true ;}
+
+else {return false;}
+
+}
+
+
 
 
 
